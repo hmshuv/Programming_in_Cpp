e31635058fdd8cadc3a0766e8591cd04f6693fb9
Himanshu Gupta <himanshugupta543210@gmail.com>
2023-07-17 21:22:46 +0530

    1st commit

---
+++ printing_a_string.cpp
@@ -0,0 +1,8 @@
+#include<iostream>         // include header file
+
+using namespace std;
+int main(){
+    cout << "Hello word  \n"; //C++ statement
+
+    return 0;
+}                           //End of example
